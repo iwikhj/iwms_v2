9e70ff414a5ef4a99d399ea50a98402272d1b1a7
iworks <iworks@DESKTOP-GCTJHQG>
2023-05-15 17:02:26 +0900

    소스 수정

--- src/main/java/com/iwi/iwms/api/auth/controller/AuthController.java
+++ src/main/java/com/iwi/iwms/api/auth/controller/AuthController.java
@@ -28,9 +28,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 
-@Slf4j
 @Tag(name = "Auth", description = "IWMS 권한 관리")
 @RequiredArgsConstructor
 @RestController
